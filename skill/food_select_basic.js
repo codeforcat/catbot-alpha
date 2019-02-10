0f35c8f8d88600a0efd1e8acb17ce1122066424e
koji shibata <sib@hvn.to>
2019-02-10 18:12:16 +0900

    food_select.js 選択方法修正

---
+++ skill/food_select_basic.js
@@ -0,0 +1,62 @@
+"use strict";
+
+module.exports = class FoodSelectBasic {
+
+  constructor() {
+    this.clear_context_on_finish = true;
+    this.required_parameter = {
+      foodbasic: {
+        message_to_confirm: {
+          type: "template",
+          altText: "他にも気になることある？",
+          template: {
+            type: "buttons",
+            text: "ご飯の基本は、こんなことがあるよ。何を知りたい？",
+            actions: [{
+                type: "postback",
+                label: "何をあげればいいの？",
+                displayText: "何をあげればいいの？",
+                data: "food_answer_dry"
+              },
+              {
+                type: "postback",
+                label: "いつあげればいいの",
+                displayText: "いつあげればいいの",
+                data: "food_answer_when"
+              },
+              {
+                type: "postback",
+                label: "おやつは？",
+                displayText: "おやつは？",
+                data: "food_answer_snack"
+              }
+            ]
+          }
+        },
+        parser: async (value, bot, event, context) => {
+          if (["food_answer_dry", "food_answer_when", "food_answer_snack"].includes(value.data)){
+            return value;
+          }
+          throw new Error();
+        },
+        reaction: async (error, value, bot, event, context) => {
+          if (error){
+            await bot.reply({
+              type: "text",
+              text: "にゃ？\nもう一度言ってほしいにゃ。"
+            });
+            await bot.init();
+          }
+        }
+      }
+    }
+  }
+
+  async finish(bot, event, context) {
+    let intent_name = context.confirmed.another_q.data;
+    console.log("*******ToiletWhyMulti*******intent_name ********: "+intent_name);
+    await bot.switch_skill({
+      name: intent_name
+    });
+  }
+};
---
+++ skill/food_select_water.js
@@ -0,0 +1,62 @@
+"use strict";
+
+module.exports = class FoodSelect {
+
+  constructor() {
+    this.clear_context_on_finish = true;
+    this.required_parameter = {
+      food: {
+        message_to_confirm: {
+          type: "template",
+          altText: "他にも気になることある？",
+          template: {
+            type: "buttons",
+            text: "ご飯の基本は、こんなことがあるよ。何を知りたい？",
+            actions: [{
+                type: "postback",
+                label: "ご飯の基本",
+                displayText: "ご飯の基本",
+                data: "food_select_basic"
+              },
+              {
+                type: "postback",
+                label: "いろんな種類があるけど？",
+                displayText: "いろんな種類があるけど？",
+                data: "food_select_donot"
+              },
+              {
+                type: "postback",
+                label: "水のあげ方は？",
+                displayText: "水のあげ方は？",
+                data: "food_select_water"
+              }
+            ]
+          }
+        },
+        parser: async (value, bot, event, context) => {
+          if (["food_select_water", "food_select_donot", "food_select_basic"].includes(value.data)){
+            return value;
+          }
+          throw new Error();
+        },
+        reaction: async (error, value, bot, event, context) => {
+          if (error){
+            await bot.reply({
+              type: "text",
+              text: "にゃ？\nもう一度言ってほしいにゃ。"
+            });
+            await bot.init();
+          }
+        }
+      }
+    }
+  }
+
+  async finish(bot, event, context) {
+    let intent_name = context.confirmed.another_q.data;
+    console.log("*******ToiletWhyMulti*******intent_name ********: "+intent_name);
+    await bot.switch_skill({
+      name: intent_name
+    });
+  }
+};
