a70863738d8af1e78e5cce4febb48a66711f9657
koji shibata <sib@hvn.to>
2019-02-11 11:57:04 +0900

    food_answer_whichdry 内容作成

---
+++ skill/food_answer_whichdry.js
@@ -0,0 +1,52 @@
+'use strict';
+// 一問一答形式
+module.exports = class FoodAnswerDry {
+  async begin(bot, event, context){
+    await bot.queue({
+      type: "text",
+      text: "ネコの年齢や体調に合ったドライを選んでね。"
+    });
+  }
+
+  constructor() {
+    this.clear_context_on_finish = true;
+    this.required_parameter = {
+      answer: {
+        message_to_confirm: {
+          type: "template",
+          altText: "もっと質問あるニャ？",
+          template: {
+            type: "buttons",
+            text: "もっと質問あるニャ？",
+            actions: [{
+                type: "postback",
+                label: "はい",
+                data: "はい"
+              },
+              {
+                type: "postback",
+                label: "いいえ",
+                data: "いいえ"
+              }
+            ]
+          }
+        }
+      },
+    };
+  }
+
+  // パラメーターが全部揃ったら実行する処理を記述します。
+  async finish(bot, event, context) {
+    console.log(context.confirmed);
+    let intent_name = "food_quiz_noteat";
+    if (context.confirmed.answer.data == "はい") {
+      await bot.reply({
+        type: "text",
+        text: "どんなことが聞きたい？"
+      });
+    }
+    await bot.switch_skill({
+      name: intent_name
+    });
+  }
+};
