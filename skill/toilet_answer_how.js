004b3858abfa5e213c7d50e9cb96c603e7afceb7
koji shibata <sib@hvn.to>
2019-02-11 18:21:45 +0900

    toilet_answer_how replayからquene

--- skill/toilet_answer_how.js
+++ skill/toilet_answer_how.js
@@ -7,7 +7,7 @@ module.exports = class ToiletAnswerHow {
       text: "子ネコの場合、トイレしそうな時に、猫砂に連れて行って、おしりをポンポン。"
     });
 
-    await bot.reply({
+    await bot.queue({
       type: "text",
       text: "大人のネコの場合、トイレをいつもきれいにして、人目につかないところに置いてみよう。ダンボールで囲ってみるのも効果あり。"
     });
