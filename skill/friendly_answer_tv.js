f074043cc2ad760b4ce6a0bc27e04777ac828175
Kazuki Imamura <kaizumaki@gmail.com>
2019-02-12 17:12:12 +0900

    仲良くなりたいテーマのスキルを追加

--- skill/friendly_answer_tv.js
+++ skill/friendly_answer_tv.js
@@ -8,7 +8,7 @@ module.exports = class FriendlyAnswerTv {
   async finish(bot, event, context){
     await bot.reply({
       type: "text",
-      text: "猫は鏡に映った何かを認識することはできます。鏡の中に映った何かに反応して威嚇してみたり、話かけてみたり、一緒に遊ぶような素振りを見せたりします。しかし、鏡の中に映るのが自分だと猫は理解することができません。"
+      text: "テレビの見えるネコと見えないネコがいるみたいです。見えていてもただの画像だと分かって興味がないだけかも知れませんが。"
     });
   }
 };
