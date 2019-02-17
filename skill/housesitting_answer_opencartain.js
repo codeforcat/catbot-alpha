8938626a64d5eb7b5594168f3cd859ea229d2209
koji shibata <sib@hvn.to>
2019-02-17 15:44:06 +0900

    お留守番もっと

--- skill/housesitting_answer_opencartain.js
+++ skill/housesitting_answer_opencartain.js
@@ -1,14 +1,77 @@
-"use strict";
+'use strict';
 
 module.exports = class HousesittingAnswerOpencartain {
-  constructor(){
-    this.clear_context_on_finish = true;
-  }
-
-  async finish(bot, event, context){
-    await bot.reply({
+  async begin(bot, event, context){
+    await bot.queue({
       type: "text",
       text: "ネコは窓辺で景色を眺めるのが好きです。小鳥や虫など、ネコの好奇心をくすぐるものがいっぱい。でも、外に出たいわけではありません。だから、カーテンは開けておいてあげてね。"
     });
+  }
+
+  constructor() {
+    this.clear_context_on_finish = true;
+    this.required_parameter = {
+      another_q: {
+        message_to_confirm: {
+          type: "template",
+          altText: "もっと質問あるにゃ？",
+          template: {
+            type: "confirm",
+            text: "もっと質問あるにゃ？",
+            actions: [
+              {
+                type: "postback",
+                label: "はい",
+                displayText: "はい",
+                data: "はい"
+              },
+              {
+                type: "postback",
+                label: "いいえ",
+                displayText: "いいえ",
+                data: "いいえ"
+              }
+            ]
+          }
+        },
+        parser: async (value, bot, event, context) => {
+          if (["はい", "いいえ"].includes(value.data)){
+            return value;
+          }
+          throw new Error();
+        },
+        reaction: async (error, value, bot, event, context) => {
+          if (error){
+            await bot.reply({
+              type: "text",
+              text: "にゃ？\nもう一度言ってほしいにゃ。"
+            });
+            await bot.init();
+          }
+        }
+      }
+    }
+  }
+
+  async finish(bot, event, context) {
+    console.log(context.confirmed);
+    let intents = ["housesitting_mame_room","","","","",""];
+    let intent_name = intents[Math.floor(Math.random()*intents.length)];
+    if (context.confirmed.another_q.data == "はい") {
+      await bot.reply({
+        type: "text",
+        text: "どんなことが聞きたい？"
+      });
+    }
+    if (intent_name == "") {
+      await bot.reply({
+        type: "text",
+        text: "また来てね！"
+      });
+    }else{
+      await bot.switch_skill({
+        name: intent_name
+      });
+    }
   }
 };
