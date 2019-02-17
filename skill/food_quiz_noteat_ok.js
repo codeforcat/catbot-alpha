17825bafc05330c82cb7a60469283e8821e690f0
koji shibata <sib@hvn.to>
2019-02-17 17:21:04 +0900

    クイズの最後にもっと質問あるを削除

--- skill/food_quiz_noteat_ok.js
+++ skill/food_quiz_noteat_ok.js
@@ -7,71 +7,11 @@ module.exports = class FoodQuizNoEatOK {
       text: "ピンポン！正解です。\n玉ねぎ、ネギ、にんにく、チョコレート、イカなど、人間の食べ物は塩気が多いのでだめです。あと、生ものも避けようね。"
     });
   }
-
-  constructor() {
-    this.clear_context_on_finish = true;
-    this.required_parameter = {
-      another_q: {
-        message_to_confirm: {
-          type: "template",
-          altText: "もっと質問あるにゃ？",
-          template: {
-            type: "confirm",
-            text: "もっと質問あるにゃ？",
-            actions: [
-              {
-                type: "postback",
-                label: "はい",
-                displayText: "はい",
-                data: "はい"
-              },
-              {
-                type: "postback",
-                label: "いいえ",
-                displayText: "いいえ",
-                data: "いいえ"
-              }
-            ]
-          }
-        },
-        parser: async (value, bot, event, context) => {
-          if (["はい", "いいえ"].includes(value.data)){
-            return value;
-          }
-          throw new Error();
-        },
-        reaction: async (error, value, bot, event, context) => {
-          if (error){
-            await bot.reply({
-              type: "text",
-              text: "にゃ？\nもう一度言ってほしいにゃ。"
-            });
-            await bot.init();
-          }
-        }
-      }
-    }
-  }
-
   async finish(bot, event, context) {
     console.log(context.confirmed);
-    let intents = ["food_mame_water","food_mame_milk","","","",""];
-    let intent_name = intents[Math.floor(Math.random()*intents.length)];
-    if (context.confirmed.another_q.data == "はい") {
-      await bot.reply({
-        type: "text",
-        text: "どんなことが聞きたい？"
-      });
-    }
-    if (intent_name == "") {
-      await bot.reply({
-        type: "text",
-        text: "また来てね！"
-      });
-    }else{
-      await bot.switch_skill({
-        name: intent_name
-      });
-    }
+    await bot.reply({
+      type: "text",
+      text: "また来てね！"
+    });
   }
 };
