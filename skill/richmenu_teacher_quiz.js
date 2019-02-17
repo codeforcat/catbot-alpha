0e2b68ac055e627a312026b8e63d2de5ae21287b
Kazuki Imamura <kaizumaki@gmail.com>
2019-02-17 15:15:35 +0900

    richmenuの「先生と遊ぼう」（クイズ）のスキルを追加

---
+++ skill/richmenu_teacher_quiz.js
@@ -0,0 +1,24 @@
+"use strict";
+
+module.exports = class RichmenuTeacherQuiz {
+  constructor() {
+    this.clear_context_on_finish = true;
+  }
+
+  async finish(bot, event, context) {
+    const intents = [
+      "food_quiz_noteat",
+      "food_quiz_plate",
+      "toilet_quiz_do",
+      "enviroment_quiz_like",
+      "enviroment_quiz_notroom",
+      // "housesitting_quiz_season",
+      "aruaru_quiz_sign"
+    ];
+    let random_intent = Math.floor(Math.random() * intents.length);
+
+    await bot.switch_skill({
+      name: intents[random_intent]
+    });
+  }
+};
